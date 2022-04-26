49265d3dd11567e48705d6b4f2cc7771dd2b73e0
alisson4 <104461212+alisson4@users.noreply.github.com>
2022-04-26 12:30:35 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("button").click(function() {
+    let name = $(".question1").val();
+    let faveIcecream = $(".question2").val();
+    let times = $(".question3").val();
+    //Write code below!
+    $().append();
+    $(".prediction").append(name + " your favorite ice cream is " + faveIcecream);
+
+});
